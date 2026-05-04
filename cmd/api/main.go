f936018d340b3b6347697ea63d8eca0b92c274e1
kevalsabhani <kevalsabhani@gmail.com>
2026-05-04 23:11:41 +0530

    Initial setup and add user routes

---
+++ cmd/api/main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// Models
+type User struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+type Note struct {
+	ID      int
+	UserID  int
+	Title   string
+	Content string
+}
+
+// In-memory stores
+var userStore = map[int]User{
+	1: User{1, "Alice", "alice@ex.com"},
+	2: User{2, "Bob", "bob@ex.com"},
+}
+
+var noteStore = map[int]Note{
+	1: Note{1, 2, "keeper api key", "fjalkdfjldfjlsjf"},
+	2: Note{2, 1, "aws secrets", "kadsfjdslfjlskdfjlksd"},
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+
+	// Routes
+	r.Get("/", handleHome)
+	r.Get("/health", handleHealth)
+
+	// User routes
+	r.Post("/users", handleCreateUser)
+	r.Get("/users", handleListUsers)
+	r.Get("/users/{id}", handleGetUser)
+	r.Put("/users/{id}", handleUpdateUser)
+	r.Delete("/users/{id}", handleDeleteUser)
+
+	// 404 handler
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "endpoint not found",
+		})
+	})
+
+	fmt.Println("Server Running on :3000")
+	http.ListenAndServe(":3000", r)
+}
+
+// Handlers
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "welcome to keeper APIs",
+		"version": "0.0.1",
+	})
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "healthy"})
+}
+
+func handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "invalid request payload",
+		})
+		return
+	}
+
+	newID := len(userStore) + 1
+	userStore[newID] = User{newID, user.Name, user.Email}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": userStore[newID],
+	})
+}
+
+func handleListUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	userList := make([]User, 0, len(userStore))
+	for _, user := range userStore {
+		userList = append(userList, user)
+	}
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": userList,
+	})
+}
+
+func handleGetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	user, exists := userStore[id]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "user not found",
+		})
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": user,
+	})
+}
+
+func handleUpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	var payload User
+	err = json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "invalid request payload",
+		})
+		return
+	}
+
+	user, exists := userStore[id]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "user not found",
+		})
+		return
+	}
+
+	user.Name = payload.Name
+	user.Email = payload.Email
+
+	userStore[id] = user
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "User updated.",
+	})
+}
+
+func handleDeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "invalid user ID",
+		})
+		return
+	}
+
+	_, exists := userStore[id]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "user not found",
+		})
+		return
+	}
+
+	delete(userStore, id)
+	w.WriteHeader(http.StatusNoContent)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kevalsabhani/keeper
+
+go 1.25.4
+
+require github.com/go-chi/chi/v5 v5.2.5
